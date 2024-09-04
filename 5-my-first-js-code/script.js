d8a4c1e6de062991a72c75dc2a191640a6223354
hussainajhar8 <hussainajhar8@gmail.com>
2024-09-04 15:38:45 +0100

    learning js

---
+++ 5-my-first-js-code/script.js
@@ -0,0 +1,15 @@
+console.log("Hello");
+
+var flower = "rose";
+
+console.log(flower);
+
+const unchangeable_variable = "Ajhar";
+
+console.log(unchangeable_variable);
+
+let this_change = "blue";
+
+this_change = "red";
+
+console.log(this_change);
