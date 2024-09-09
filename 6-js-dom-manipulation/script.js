89b4a5f907dc5eaf09601a8b5c682ee0188e6b43
hussainajhar8 <hussainajhar8@gmail.com>
2024-09-09 15:41:59 +0100

    learning dom manipulation

---
+++ 6-js-dom-manipulation/script.js
@@ -0,0 +1,9 @@
+// Selectors:
+
+// Single element selectors
+console.log(document.getElementById("my-form"));
+console.log(document.querySelector("h1"));
+
+// Multiple element selectors
+console.log(document.querySelectorAll("h1"));
+console.log(document.getElementsByClassName("page-content"));
