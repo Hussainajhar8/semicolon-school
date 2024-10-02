538aa673f342562f14c4d60d7589158982acd4da
hussainajhar8 <hussainajhar8@gmail.com>
2024-10-02 18:21:35 +0100

    update fetch-api

---
+++ 9-fetch-api/script.js
@@ -0,0 +1,35 @@
+const URL = `https://dummyjson.com/posts`
+const containerDiv = document.getElementById("container");
+
+const loadingH2 = document.createElement('h2')
+loadingH2.textContent = "Loading..."
+containerDiv.appendChild(loadingH2)
+
+fetch(URL)
+.then(response => response.json())
+.then(data => {
+  containerDiv.removeChild(loadingH2)
+  console.log(data);
+  
+  data.posts.forEach(post => {
+    const postDiv = document.createElement("div");
+
+    const titleH2 = document.createElement("h2");
+    const bodyP = document.createElement("p");
+    const likesSpan = document.createElement("span")
+    const dislikesSpan = document.createElement("span")
+    
+    titleH2.textContent = post.title;
+    bodyP.textContent = post.body;
+    likesSpan.textContent = `Likes: ${post.reactions.likes} \n/ `;
+    dislikesSpan.textContent = `Dislikes: ${post.reactions.dislikes}`;
+
+    postDiv.appendChild(titleH2);
+    postDiv.appendChild(bodyP);
+    postDiv.appendChild(likesSpan);
+    postDiv.appendChild(dislikesSpan);
+
+    containerDiv.appendChild(postDiv);
+  });
+
+});
