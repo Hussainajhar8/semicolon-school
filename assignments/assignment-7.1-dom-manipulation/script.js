381ca5df0a024f5e4a0fabe1a7c325f049eb47a2
hussainajhar8 <hussainajhar8@gmail.com>
2024-10-08 14:56:15 +0100

    update dom manipulation assignment

--- assignments/assignment-7.1-dom-manipulation/script.js
+++ assignments/assignment-7.1-dom-manipulation/script.js
@@ -3,73 +3,79 @@ function addTask() {
   var list = document.getElementById('taskList');
   var newItem = document.createElement('li');
 
-// Step 1a
+// Step 1a - Check if the input is empty before adding a task
   if (input.value.trim() === "") {
     console.log("Empty string, please add a task.");
     return;
 }
 
-// Step 1b
+// Step 1b - Create a text node instead of using innerHTML
   const text = document.createTextNode(input.value);
   newItem.append(text);
 
-// Step 1c
+// Step 1c - Add a delete button next to each new task
 
-  const deleteButton = document.createElement('button');
-  const deleteButtonText = document.createTextNode("Delete")
-  deleteButton.appendChild(deleteButtonText);
+  // const deleteButton = document.createElement('button'); - Creates the button element
+  // const deleteButtonText = document.createTextNode("Delete") - Creates the text for the button
+  // deleteButton.appendChild(deleteButtonText); - Adds the text to the button
   
-  newItem.appendChild(deleteButton)
-  list.append(newItem)
+  // newItem.appendChild(deleteButton) - Adds the button to the item
 
-// Step 2e
-  newItem.addEventListener('click', function() {
-    completeTask(newItem)
-  });
+  createDeleteButton(newItem) // Calls the createDeleteButton function which was created for the querySelectorAll task
+  list.append(newItem) // Adds the new task to the list
 
-// Step 3
-  deleteButton.addEventListener('click', function(event) {
-    event.stopPropagation(); // Prevent the click event from bubbling up to the <li>
-    newItem.remove()
-  })
+// // Step 2e - Make it listen to a click event on each task
+//   newItem.addEventListener('click', function() {
+//     completeTask(newItem)
+//   });
 
-// Step 5
+// // Step 3
+//   deleteButton.addEventListener('click', function(event) { // Step 3g - Make it listen to the delete button for each task
+//     event.stopPropagation(); // Prevent the click event from bubbling up to the <li>
+//     newItem.remove() // Step 3f - It should remove the task from the list
+//   })
 
-  selectAllTasks();
+  selectAllTasks(); // Called function from step 5
 
   input.value = "";
 
 }
 
-// This function is not used yet - sike
+// This function is not used yet - sike (It should toggle the ‘completed’ class on and off)
 
 function completeTask(task) {
-// Step 2d - change add to toggle
-  task.classList.toggle('completed');
+  // Step 2d - change add to toggle
+    task.classList.toggle('completed');
 }
-
-// Step 5
+  
+// Step 5 - Use `querySelectorAll` to select all tasks and loop through them to add event listeners
 
 function selectAllTasks() {
   const tasks = document.querySelectorAll('#taskList li');
   tasks.forEach(function(task) {
     task.addEventListener('click', function() {
-      completeTask(task);
+      completeTask(task); // Call completeTask on click
     });
+
+    // Create and append delete button for each task
     createDeleteButton(task);
   });
 }
 
-// function createDeleteButton(task) {
-//   const deleteButton = document.createElement('button');
-//   const deleteButtonText = document.createTextNode("Delete");
-//   deleteButton.appendChild(deleteButtonText);
+// Commented Out Step 1c, 3f and 3g to make this function which'll be used by Step5 function to add a delete button to all tasks
+function createDeleteButton(task) {
+  if (task.querySelector('button')) {
+    return; // Exit the function if a delete button already exists
+  }
+  const deleteButton = document.createElement('button');
+  const deleteButtonText = document.createTextNode("Delete");
+  deleteButton.appendChild(deleteButtonText);
   
-//   // Add click event listener to the delete button
-//   deleteButton.addEventListener('click', function(event) {
-//       event.stopPropagation(); // Prevent click on delete button from marking the task as complete
-//       task.remove(); // Remove the task from the list
-//   });
+  // Add click event listener to the delete button
+  deleteButton.addEventListener('click', function(event) {
+      event.stopPropagation(); // Prevent click on delete button from marking the task as complete
+      task.remove(); // Remove the task from the list
+  });
 
-//   task.appendChild(deleteButton); // Append the delete button to the task (li)
-// }
+  task.appendChild(deleteButton); // Append the delete button to the task (li)
+}
---
+++ assignments/assignment-9.1-fetch-api/script.js
@@ -0,0 +1,26 @@
+const URL = `https://api.thecatapi.com/v1/images/search?limit=33`;
+
+fetch(URL)
+.then((response) => { 
+  return response.json();
+})
+.then((data) => {
+  let imagesData = data;
+  imagesData.map(function(imageData) {
+    
+    let image = document.createElement('img');
+    //use the url from the image object
+    image.src = `${imageData.url}`;
+        
+    let gridCell = document.createElement('div');
+    gridCell.classList.add('col');
+    gridCell.classList.add('col-lg');
+    gridCell.appendChild(image)
+      
+    document.getElementById('grid').appendChild(gridCell);
+    
+    });
+})
+.catch(function(error) {
+   console.log(error);
+});
