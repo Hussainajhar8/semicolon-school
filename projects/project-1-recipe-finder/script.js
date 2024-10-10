10f73ded1eacf9dc2842c0e2a1c4146ef21af965
hussainajhar8 <hussainajhar8@gmail.com>
2024-10-10 16:46:36 +0100

    update project

--- projects/project-1-recipe-finder/script.js
+++ projects/project-1-recipe-finder/script.js
@@ -14,6 +14,23 @@ function findRecipe() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+
+      containerDiv.innerHTML = "";
+
+      data.forEach((recipe) => {
+        const recipeDiv = document.createElement("div");
+        const recipeTitle = document.createElement("h2");
+        const recipeImage = document.createElement("img");
+
+        recipeTitle.textContent = recipe.title;
+        recipeImage.src = recipe.image;
+        recipeImage.alt = recipe.title;
+
+        recipeDiv.appendChild(recipeTitle);
+        recipeDiv.appendChild(recipeImage);
+
+        containerDiv.appendChild(recipeDiv);
+      });
     })
     .catch((error) => {
       console.error("Error fetching recipes:", error);
